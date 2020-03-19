f9f1cbf9cb686cf7b04f59f08e94129371aed181
flychensc <flychensc@163.com>
2020-03-19 23:16:46 +0800

    compiler error.

--- src/erbio.erl
+++ src/erbio.erl
@@ -4,9 +4,14 @@
 
 -behaviour(gen_server).
 
+-include("erbio.hrl").
+
 %% API
 -export([start/0, start/1, start/2, start/3, stop/0]).
--export([create/1, cleanup/1, handshake/1]).
+-export([create/1, cleanup/1]).
+-export([handshake/1, is_init_finished/1]).
+-export([ssl_write/2, ssl_read/1]).
+-export([bio_write/2, bio_read/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -record(state, {port}).
 
@@ -47,6 +52,31 @@ cleanup(Id) ->
 handshake(Id) ->
     gen_server:call(?MODULE, {handshake, Id}).
 
+%% is init finished
+-spec is_init_finished(integer()) -> {ok, true | false} | fail.
+is_init_finished(Id) ->
+    gen_server:call(?MODULE, {is_init_finished, Id}).
+
+%% do ssl_write
+-spec ssl_write(integer(), binary()) -> {ok, integer()} | wouldblock | fail.
+ssl_write(Id, Data) ->
+    gen_server:call(?MODULE, {ssl_write, Id, Data}).
+
+%% do ssl_read
+-spec ssl_read(integer()) -> {ok, binary()} | wouldblock | fail.
+ssl_read(Id) ->
+    gen_server:call(?MODULE, {ssl_read, Id}).
+
+%% do bio_write
+-spec bio_write(integer(), binary()) -> {ok, integer()} | fail.
+bio_write(Id, Data) ->
+    gen_server:call(?MODULE, {bio_write, Id, Data}).
+
+%% do bio_read
+-spec bio_read(integer()) -> {ok, binary()} | fail.
+bio_read(Id) ->
+    gen_server:call(?MODULE, {bio_read, Id}).
+
 init(Args) ->
     Command = genCommand(Args),
     % io:format("Command:~s~n", [Command]),
@@ -56,27 +86,135 @@ init(Args) ->
 handle_call(stop, _From, State) ->
    {stop, normal, stopped, State};
 
-handle_call(_Request, _From, State) ->
-    {reply, ok, State}.
-
-handle_cast({reg, Handler}, State) ->
-    #state{handlers=Handlers} = State,
-   {noreply, #state{handlers=lists:append(Handlers, [Handler])}};
-
-handle_cast({send, Packet}, State) ->
+handle_call({create, client}, _From, State) ->
     #state{port=Port} = State,
-    Port ! {self(), {command, Packet}},
-   {noreply, State};
+    Port ! {self(), {command, <<?CMD_CREATE_CLIENT>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK, Id:16/big-integer>> ->
+               {reply, {ok, Id}, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call({create, server}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_CREATE_SERVER>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK, Id:16/big-integer>> ->
+               {reply, {ok, Id}, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call({cleanup, Id}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_CLEANUP, Id:16/big-integer>>}},
+    receive
+      {Port, {data, Data}} ->
+         {reply, ok, State}
+    end;
+
+handle_call({handshake, Id}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_HANDSHAKE, Id:16/big-integer>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK>> ->
+               {reply, ok, State};
+
+            <<?RET_WOULD_BLOCK>> ->
+               {reply, wouldblock, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call({is_init_finished, Id}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_IS_INIT_FINISHED, Id:16/big-integer>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK, 1>> ->
+               {reply, {ok, true}, State};
+
+            <<?RET_OK, 0>> ->
+               {reply, {ok, false}, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call({ssl_write, Id, Data}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_SSL_WRITE, Id:16/big-integer, Data/binary>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK, Written>> ->
+               {reply, {ok, Written}, State};
+
+            <<?RET_WOULD_BLOCK, 0>> ->
+               {reply, wouldblock, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call({ssl_read, Id}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_SSL_READ, Id:16/big-integer>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK, ReadData/binary>> ->
+               {reply, {ok, ReadData}, State};
+
+            <<?RET_WOULD_BLOCK, 0>> ->
+               {reply, wouldblock, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call({bio_write, Id, Data}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_BIO_WRITE, Id:16/big-integer, Data/binary>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK, Written>> ->
+               {reply, {ok, Written}, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call({bio_read, Id}, _From, State) ->
+    #state{port=Port} = State,
+    Port ! {self(), {command, <<?CMD_BIO_READ, Id:16/big-integer>>}},
+    receive
+      {Port, {data, Data}} ->
+         case Data of
+            <<?RET_OK, ReadData/binary>> ->
+               {reply, {ok, ReadData}, State};
+
+            <<?RET_FAIL>> ->
+               {reply, fail, State}
+    end;
+
+handle_call(_Request, _From, State) ->
+   {reply, ok, State}.
 
 handle_cast(_Msg, State) ->
    {noreply, State}.
-
-handle_info({_Port, {data, Data}}, State) ->
-    #state{handlers=Handlers} = State,
-    Packet = list_to_binary(Data),
-    % io:format("recv ~w bytes~n", [byte_size(Packet)]),
-    lists:foreach(fun(Handler)-> Handler(Packet) end, Handlers),
-   {noreply, State};
 
 handle_info(_Info, State) ->
    {noreply, State}.
---
+++ src/erbio_sample.erl
@@ -0,0 +1,9 @@
+%%% A sample to use erpbio
+
+-module(erbio_sample).
+
+
+-export([start/0]).
+
+start() ->
+    erbio:start().
